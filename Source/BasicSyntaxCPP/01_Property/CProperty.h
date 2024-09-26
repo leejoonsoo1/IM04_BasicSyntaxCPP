95244c0b5038164c808d54499cb99944b50de8cd
leejoonsoo1 <leejunsu_@nate.com>
2024-09-26 19:52:17 +0900

    2024 09 26
    UnrealEngine CPP 시작

---
+++ Source/BasicSyntaxCPP/01_Property/CProperty.h
@@ -0,0 +1,22 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CProperty.generated.h"
+
+UCLASS()
+class BASICSYNTAXCPP_API ACProperty : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ACProperty();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+};
